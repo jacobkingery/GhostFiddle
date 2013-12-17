c0a47aa74a702523b60df7c540802219ec021225
bren525 <bren525@gmail.com>
2013-12-17 01:20:49 -0500

    threaded timing

--- subProcessedDriver.py
+++ subProcessedDriver.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python
 from Adafruit_PWM_Servo_Driver import PWM
-from time import clock
+import threading
 import sys
 pwm = PWM(0x40, debug=True)
 noteLen = .5
 breakLen = .1
-
 def playSong(song):
 	#f = open('SongsPlayed.txt','a')
 	#f.write(song + '\n')
 	#f.close()
 	song = song[:len(song)-1].split(';')
 	song = [glass+'0' for glass in song]
-	for i in range(len(song[0])):
+	global i
+	i=0
+	def playPause():
+		global i
+		i=i+1
 		for j in range(len(song)):
-	#		print(j, int(song[j][i]))
+#			print(j, int(song[j][i]), i)
 			pwm.setPWM(j, 0, 3000*int(song[j][i]))
-		wait(noteLen*(not i%2) + breakLen*(i%2))
-	wait(4)
+		if(i<len(song[0])-1):
+			threading.Timer(breakLen,playNote).start()
 
-def wait(duration):
-	start = clock()
-	while(clock() - start < duration):
-		pass
+	def playNote():
+		global i
+		i=i+1
+		for j in range(len(song)):
+#			print(j, int(song[j][i]), i)
+			pwm.setPWM(j, 0, 3000*int(song[j][i]))
+		if(i<len(song[0])-1):
+			threading.Timer(noteLen,playPause).start()
+
+	playNote()
+
 
 if __name__ == "__main__":
 	print("Driver Ran")
